868a0a0602af43fec7f5e66bf5a3b8ca4c8ad8fb
Vincent Waters <vwaters18@gmail.com>
2019-03-13 16:43:48 -0500

    Created Project Solution in VS.

---
+++ FirstWebsite/FirstWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
